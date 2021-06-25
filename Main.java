45f9a756440b9c3b34b4b99efc549463f7df74d7
lantanakamara <asadatnish@gmail.com>
2021-06-25 05:38:24 -0400

    add the addLast method which adds a node at the end of the list

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
     var list = new linkedlylist();
+    // list.addFirst(10);
+    // list.addFirst(20);
+    // list.addFirst(30);
     list.addFirst(10);
-    list.addFirst(20);
-    list.addFirst(30);
+    list.addLast(20);
+    list.addLast(30);
+    list.addLast(40);
+    // list.addFirst(2);
     list.show();
   }
 }
--- linkedlylist.java
+++ linkedlylist.java
@@ -9,18 +9,41 @@ public class linkedlylist {
   private Node Tail;
 
   public void addFirst(int d) {
+    // create new node
     var node = new Node();
     node.Data = d;
+    // if there is no nodes already in the list
     if (Head == null) {
+      // point the head to the new node
       Head = node;
+      // point the tail to the new node
       Tail = node;
     }
 
     else {
+      // if there is a node already
+      // point the Next of the new node to the head
       node.Next = Head;
+      // then move the head to point to the newly created node
       Head = node;
     }
 
+  }
+
+  // addLast Method
+  public void addLast(int data) {
+    // creae new node
+    var node = new Node();
+    node.Data = data;
+
+    Node current = Head;
+
+    while (current.Next != null) {
+
+      current = current.Next;
+    }
+    current.Next = node;
+    Tail = node;
   }
 
   public void show() {
